0ca4a2fabd518e65d28dbf34a210980fe1374cd1
Author:    zixuanzh <zzhangzixuan@gmail.com>
2018-05-28 18:13:25 +0000

    get account view working

--- apps/explorer_web/lib/explorer_web/views/account_view.ex
+++ apps/explorer_web/lib/explorer_web/views/account_view.ex
@@ -3,13 +3,16 @@ defmodule ExplorerWeb.AccountView do
 
   import Math.Enum, only: [mean: 1]
 
-  alias Explorer.Chain.{Account}
+  alias Explorer.Chain.{Address}
 
   @dialyzer :no_match
 
-  def formatted_last_update(%Account{updated_at: timestamp}) do
+  def formatted_last_update(%Address{updated_at: timestamp}) do
     Timex.format!(timestamp, "%b-%d-%Y %H:%M:%S %p %Z", :strftime)
   end
 
+  def format_balance(%Address{fetched_balance: balance}) do
+    format_wei_value(balance, :ether)
+  end
 end
 
