e11aa755844b67b16fde31021157de8de8933a91
zixuanzh <zixuanzh@seas.upenn.edu>
2018-05-28 09:56:56 -0300

    add account view

--- apps/explorer_web/lib/explorer_web/views/account_view.ex
+++ apps/explorer_web/lib/explorer_web/views/account_view.ex
@@ -7,5 +7,9 @@ defmodule ExplorerWeb.AccountView do
 
   @dialyzer :no_match
 
+  def formatted_last_update(%Account{updated_at: timestamp}) do
+    Timex.format!(timestamp, "%b-%d-%Y %H:%M:%S %p %Z", :strftime)
+  end
+
 end
 
