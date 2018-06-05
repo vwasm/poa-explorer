273377441e3d534320a7910db8ab160b6c6ea1c5
zixuanzh <zzhangzixuan@gmail.com>
2018-06-05 18:15:00 +0000

    codeview routing working

--- apps/explorer_web/lib/explorer_web/controllers/account_controller.ex
+++ apps/explorer_web/lib/explorer_web/controllers/account_controller.ex
@@ -9,8 +9,19 @@ defmodule ExplorerWeb.AccountController do
     render(conn, "index.html", accounts: accounts)
   end
 
-  def show(conn, %{"id" => number, "locale" => locale}) do
-    redirect(conn, to: account_code_path(conn, :index, locale, number))
-  end
+  def show(conn, %{"id" => account_id}) do
+    IO.inspect("from account_controller")
+    IO.inspect(conn)
+    render(
+      conn,
+      "show.html",
+      code: "ewasm classic yo"
+    )
+    else
+      :error ->
+        not_found(conn)
 
+      {:error, :not_found} ->
+        not_found(conn)
+    end
 end
